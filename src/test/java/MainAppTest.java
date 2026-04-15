29d37ef6897e82081f3608a5325c7b4ac9f3711a
23MIS0270 <mahalakshmi.k2023b@vitstudent.ac.in>
2026-04-15 22:38:27 +0530

    Create MainAppTest.java

---
+++ src/test/java/MainAppTest.java
@@ -0,0 +1,14 @@
+public class MainAppTest {
+    public static void main(String[] args) {
+
+        System.out.println("Running basic test...");
+
+        int result = 10 + 20;
+
+        if (result == 30) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+    }
+}
