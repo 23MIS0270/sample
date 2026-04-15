0eca75a42a0e4f9efb27be5ecce1da0b8590dbea
23MIS0270 <mahalakshmi.k2023b@vitstudent.ac.in>
2026-04-15 22:37:54 +0530

    Create MainApp.java

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,11 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Generic Project");
+
+        int a = 10, b = 20;
+        System.out.println("Sum: " + (a + b));
+
+    }
+}
